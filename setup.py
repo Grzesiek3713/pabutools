40127438c18ce19c72889955f5762eff0a03972c
Grzesiek3713 <g.pierczynski@gmail.com>
2022-09-06 15:16:46 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pabutools',
+      version='0.0',
+      description='PB voting rules and tools for reading Pabulib datasets',
+      packages=['pabutools'],
+      author_email='g.pierczynski@gmail.com',
+      zip_safe=False)
