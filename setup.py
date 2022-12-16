3580e29f4d8347d04457dc1f8158512894ad7b8a
Grzesiek3713 <g.pierczynski@gmail.com>
2022-12-16 15:13:05 +0100

    Update to PyPi 0.5

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
 
 setup(name='pabutools',
-      version='0.2',
+      version='0.5',
       description='Implementation of PB voting rules and tools for reading Pabulib datasets',
+      long_description=(this_directory / "README.md").read_text(),
+      long_description_content_type='text/markdown',
+      readme = "README.md",
       packages=['pabutools'],
       author_email='g.pierczynski@gmail.com',
       zip_safe=False)
